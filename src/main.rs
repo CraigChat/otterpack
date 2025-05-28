a7865cfaa5b458b129a52f70296596845aaf555e
Snazzah <me@snazzah.com>
2025-05-28 01:11:09 -0500

    move files around

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use otterpack_exe::TemplateApp;
+use egui::IconData;
+use otterpack::TemplateApp;
 
 fn main() -> eframe::Result {
   env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -12,12 +13,8 @@ fn main() -> eframe::Result {
   let native_options = eframe::NativeOptions {
     viewport: egui::ViewportBuilder::default()
       .with_inner_size([500.0, 200.0])
-      .with_min_inner_size([500.0, 200.0]),
-    // .with_icon(
-    //     // NOTE: Adding an icon is optional
-    //     eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
-    //         .expect("Failed to load icon"),
-    // ),
+      .with_min_inner_size([500.0, 200.0])
+      .with_icon(IconData::default()),
     ..Default::default()
   };
   eframe::run_native(
